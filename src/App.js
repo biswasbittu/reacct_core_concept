e5ade91817ccdffd5b65e642132d81eac8fac0ca
biswasbittu <biswasbittu7415@yahoo.com>
2023-03-22 00:01:26 +0530

    random data shown

--- src/App.js
+++ src/App.js
@@ -1,31 +1,104 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
+// const products=[
+//   {name:'Laptop', price: 30000},
+//   {name:'Phone', price: 35000},
+//   {name:'watch', price: 3500},
+//   {name:'tablate', price: 40000},
+// ]
+
+// 
+// {
+//   products.map(product=><Products name={product.name} price={product.price}></Products>)
+// }
 
 function App() {
-  const products=[
-    {name:'Laptop', price: 30000},
-    {name:'Phone', price: 35000},
-    {name:'watch', price: 3500},
-    {name:'tablate', price: 40000},
-  ]
+
   return (
     <div className="App">
-      {
-        products.map(product=><Products name={product.name} price={product.price}></Products>)
-      }
-     
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
+
     </div>
   );
 }
 
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
 
-function Products(props){
-return(
-  <div className='product'>
-    <h3>Name:{props.name}</h3>
-    <p>Price:{props.price}</p>
-  </div>
-)
+  },[])
+
+
+  return (
+    <div>
+      <h1>External Users</h1>
+      
+      <p>{
+        users.map(user=><User name={user.name} email={user.email} ></User>)
+        }</p>
+    </div>
+  )
 }
+
+function User(props){
+  return(
+    <div>
+      <h3>
+        Name{props.name}
+      </h3>
+      <h4>Email:{props.email}</h4>
+    </div>
+  )
+}
+
+// function Counter(){
+//   const [count, setCount]=useState(0);
+
+//   const increaseCount=()=>setCount(count+1);
+
+//   const decreaseCount=()=>setCount(count-1);
+
+//   // {
+//       // const newCount=count+1;
+
+//     // }
+
+//   // const increaseCount=()=>{
+//   //   const newCount=count+1;
+//   //   setCount(newCount)
+//   // }
+
+//   return(
+//     <div>
+//       <h1>Count:{count}</h1>
+//       <button onClick={increaseCount}>Increase</button>
+//       <button onClick={decreaseCount}>Decrease</button>
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+
+// function Products(props){
+//   return(
+//     <div className='product'>
+//       <h3>Name:{props.name}</h3>
+//       <p>Price:{props.price}</p>
+//     </div>
+//   )
+//   }
+
+
 
 export default App;
